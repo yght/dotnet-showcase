e1c270800e2224b44b084393e9e01bc59ba3a02d
Dale Hypereon <dale@sinexo.io>
2022-03-28 10:15:00 -0400

    Add comprehensive observability with OpenTelemetry and health monitoring
    - Implement OpenTelemetry distributed tracing with activity sources
    - Add health checks for SQL Server, Redis, and application status
    - Configure Jaeger for trace visualization and debugging
    - Add minimal hosting with modern .NET 6 dependency injection
    - Include Docker orchestration for full observability stack

--- MessagePlatform/MessagePlatform.API/Program.cs
+++ MessagePlatform/MessagePlatform.API/Program.cs
@@ -1,20 +1,91 @@
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
+using MessagePlatform.Infrastructure;
+using MessagePlatform.API.GraphQL;
+using MessagePlatform.API.Hubs;
+using MediatR;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using EventStore.ClientAPI;
+using MessagePlatform.Infrastructure.EventSourcing;
 
-namespace MessagePlatform.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add infrastructure services
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+// Add MediatR
+builder.Services.AddMediatR(typeof(Program).Assembly, typeof(MessagePlatform.Core.Commands.CreateMessageCommand).Assembly);
+
+// Add GraphQL
+builder.Services
+    .AddGraphQLServer()
+    .AddQueryType(d => d.Name("Query"))
+    .AddMutationType(d => d.Name("Mutation"))
+    .AddTypeExtension<MessageQueries>()
+    .AddTypeExtension<MessageMutations>()
+    .AddProjections()
+    .AddFiltering()
+    .AddSorting();
+
+// Add SignalR with Redis backplane
+builder.Services.AddSignalR()
+    .AddStackExchangeRedis(builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379");
+
+// Add EventStore
+var eventStoreConnection = EventStoreConnection.Create(
+    builder.Configuration.GetConnectionString("EventStore") ?? "ConnectTo=tcp://admin:changeit@localhost:1113");
+await eventStoreConnection.ConnectAsync();
+builder.Services.AddSingleton(eventStoreConnection);
+builder.Services.AddScoped<IEventStore, EventStoreService>();
+
+// Add OpenTelemetry
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing => tracing
+        .SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService("MessagePlatform.API"))
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation()
+        .AddConsoleExporter());
+
+// Add health checks
+builder.Services.AddHealthChecks()
+    .AddSqlServer(builder.Configuration.GetConnectionString("SqlServer") ?? "Server=(localdb)\\mssqllocaldb;Database=MessagePlatform;Trusted_Connection=True;")
+    .AddRedis(builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379");
+
+// Add CORS
+builder.Services.AddCors(options =>
 {
-    public class Program
+    options.AddDefaultPolicy(policy =>
     {
-        public static void Main(string[] args)
-        {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
-            host.Run();
-        }
-    }
+var app = builder.Build();
+
+// Configure pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
+
+app.UseHttpsRedirection();
+app.UseCors();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGraphQL();
+app.MapHub<MessageHub>("/messageHub");
+app.MapHealthChecks("/health");
+
+await app.RunAsync();
